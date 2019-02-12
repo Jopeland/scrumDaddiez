b84bec12d61d13ee97e9a8fe40a6be58ea640145
Jake Copeland <jrcopel1@asu.edu>
2019-02-12 13:50:08 -0700

    added method to validate user accounts and passwords on the server side

--- fileStorage/fileStorageServices.asmx.cs
+++ fileStorage/fileStorageServices.asmx.cs
@@ -48,8 +48,48 @@ namespace fileStorage
 
             sqlDa.Fill(sqlDt);
             return sqlDt.Rows.Count;
+        }
 
+        [WebMethod]
+        public bool VerifyCredentials(string username, string password)
+        {
+            string dbPass = "";
 
+            // grabbing connection string from config file
+            string sqlConnectString = System.Configuration.ConfigurationManager.ConnectionStrings["myDB"].ConnectionString;
+
+            // instantiating query
+            string sqlSelect = $"Select * FROM users WHERE username='{username}' OR email='{username}'";
+
+            // set up our connectino object to be ready to use our connection string
+            MySqlConnection sqlConnection = new MySqlConnection(sqlConnectString);
+            // set up command object to use our connection, and our query
+            MySqlCommand sqlCommand = new MySqlCommand(sqlSelect, sqlConnection);
+            sqlConnection.Open();
+
+            // creating a reader that will parse data returned and allow for storage into a variable
+            MySqlDataReader reader = sqlCommand.ExecuteReader();
+
+            try
+            {
+                while (reader.Read())
+                {
+                    // reading value in specific column "Password"
+                    dbPass = (string)reader["Password"];
+                }
+
+                // checking to see if password in DB matches password provided
+                if (dbPass == password)
+                    return true;
+                else
+                    return false;
+            }
+            finally
+            {
+                // terminating reader and DB connections
+                reader.Close();
+                sqlConnection.Close();
+            }
         }
     }
 }
